4d4763b0d9cc2a68bc42881a8208eccff652dc7b
Bruno Calixto <brunorctorres@gmail.com>
2026-04-10 09:06:34 -0400

    feat: add integer sum exercise (ExerciseFour)

---
+++ ExerciseFour/ExerciseFour/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace ExerciseFour
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Quantos números inteiros você vai digitar? ");
+            int quantidade = int.Parse(Console.ReadLine());
+
+            double soma = 0.0;
+
+            for (int indice = 1; indice <= quantidade; indice++)
+            {
+                Console.Write("Valor #" + indice + ": ");
+                int numero = int.Parse(Console.ReadLine());
+                soma += numero;
+            }
+
+            Console.WriteLine("Soma = " + soma);
+        }
+    }
+}
